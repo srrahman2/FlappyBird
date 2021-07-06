ca0878bc7dee01a8b3a934c69c28ddf80474c75d
srrahman2 <85581572+srrahman2@users.noreply.github.com>
2021-07-06 10:01:07 -0400

    Add files via upload

---
+++ Renderer.java
@@ -0,0 +1,12 @@
+package flappyBird;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Renderer extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        FlappyBird.flappyBird.repaint(g);
+    }
+}
